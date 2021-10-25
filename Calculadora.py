9e1c6e9167dbeb2a2dcc2c16da7bba68bd04c15f
Dorian Sudario sudario <pythondatasciencexpert@gmail.com>
2021-10-25 20:37:01 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,31 @@
+#=========================================================
+# Software
+# Dorian Sudario 
+# 2021/10/25
+#=========================================================
+
+import re
+expression = 'start'
+
+print(' Software - Calculadora')
+print('===================================')
+print('Insira a expressão a calcular!')
+print('Pode ser utilizado, virgula flutuante(ex:0.10), e as operações:')
+print('+  (Soma)')
+print('-  (Subtração)')
+print('*  (Multiplicação)')
+print('/  (Divisão)')
+print('** (Potencia)')
+print('\nq  (Para Terminar)')
+
+while expression != 'q':
+    expression= input('\nInserir expressão: ')
+    try:
+        if (re.match(r"^(\d{1,}(\.\d{1,})?(([-+/]|[*]{1,2})\d{1,}(\.\d{1,})?)+)+$",expression)):
+            print('= ', eval(expression))
+        elif expression == 'q ':
+            print('Terminando... ')    
+        else:
+            print('Expressão inválida!')
+    except:
+        print("Aconteceu qualquer situação anómala!")
